9dc05e64aaf0e0d1eee8adf3f49610f9423fbf66
peppermint-sq <sq2049576950@163.com>
2023-04-08 21:14:38 +0800

    初始化项目

---
+++ SE01/src/com/sq/Test/D06/T1.java
@@ -0,0 +1,16 @@
+package com.sq.Test.D06;
+
+public class T1 {
+    public static void main(String[] args) {
+//    定义一个方法，该方法能够找出两个小数中的较小值并返回。
+//    在主方法中调用方法进行测试。
+        System.out.println(getMin(23,678));
+    }
+    public static int getMin(int num1,int num2){
+        if(num1<num2){
+            return num1;
+        }else {
+            return num2;
+        }
+    }
+}
